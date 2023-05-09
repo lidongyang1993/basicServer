3f2b7c6c21700ef64a158f479b9c154d42a8db9d
杨望宇 <wangyu.yang@szyh.com>
2023-05-09 09:39:24 +0800

    增加查询接口

--- basicServer/settings.py
+++ basicServer/settings.py
@@ -125,6 +125,7 @@ LANGUAGE_CODE = 'zh-hans'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
