5d16f653855185e10336ed794d78d2df9b42a675
杨望宇 <wangyu.yang@szyh.com>
2023-06-13 18:23:26 +0800

    增加获取提取验证的服务

--- jobs/api_frame/basics/read_plan.py
+++ jobs/api_frame/basics/read_plan.py
@@ -54,8 +54,8 @@ def update_plan_into_module(plan_name, name, data: json):
     file = open(file_path, "w")
     for _ in read_data:
         if _["name"] == plan_name:
-            index = read_data.index(_)
-            read_data[index] = data
+            _ = data
             break
+
     file.write(json.dumps(read_data, ensure_ascii=False))
     file.close()
