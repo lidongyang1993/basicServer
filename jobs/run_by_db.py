9a93449105d627f43c6da603780e18f7ecc3303c
杨望宇 <wangyu.yang@szyh.com>
2023-07-13 18:41:11 +0800

    大修正 调整日志文件 重构执行顺序 提炼文件路径的配置文件 移出旧逻辑代码

--- jobs/run_by_db.py
+++ jobs/run_by_db.py
@@ -121,7 +121,7 @@ class StartRun:
         all_case = result.get("all")
         fail_case = result.get("fail")
         host = read_data("file_server", "host")
-        port = read_data("file_server", "port")
+        port = read_data("file_server", "port_report")
         call_url = "http://{}:{}/{}/{}".format(host, port, self.user, runner.filename)
         send_test_report(self.user, self.title, all_case, pass_case, fail_case, call_url, self.w_chat_url)
 
